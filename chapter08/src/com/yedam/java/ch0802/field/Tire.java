b3418d1e93f5c0752690714c674d922cc9985460
master <mymail@google.com>
2026-01-05 17:59:55 +0900

    클래스

---
+++ chapter08/src/com/yedam/java/ch0802/field/Tire.java
@@ -0,0 +1,7 @@
+package com.yedam.java.ch0802.field;
+
+public interface Tire {
+	
+//	abstract public void roll(); 추상 클래스가 기본이라 abstract 생략가능
+	public void roll();
+}
---
+++ chapter10/src/com/yedam/java/chap1002/ThrowsExample.java
@@ -0,0 +1,19 @@
+package com.yedam.java.chap1002;
+
+public class ThrowsExample {
+	public static void main(String[] args) {
+		try {
+			findClass();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void findClass() throws ClassNotFoundException, NullPointerException {
+		Class clazz = Class.forName("java.lang.String2");
+	}
+
+}
